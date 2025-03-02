3545d50f80bfb933c4302cbec6da178ac09c8795
Vetal <Vetal@GB550PRO>
2025-03-02 17:38:24 +0300

    up

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,86 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [episodes, setEpisodes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const starWarsUrl = "https://swapi.dev/api/films";
+
+  // функция загрузки данных
+  const loadEpisodes = async () => {
+    try {
+      const response = await axios.get(starWarsUrl);
+      setEpisodes(response.data.results);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Загрузка данных рендеринге страницы
+  useEffect(() => {
+    loadEpisodes();
+  }, []);
+
+  // Обработчик клика по эпизоду показываем/скрываем подробные данные. ... - копируются все свойства текущего объекта
+  const episodeClick = (id) => {
+    const updatedEpisodes = episodes.map((episode) => {
+      if (episode.episode_id === id) {
+
+        return { ...episode, showAllInfo: !episode.showAllInfo };
+      }
+      return { ...episode, showAllInfo: false };
+    });
+    setEpisodes(updatedEpisodes);
+  };
+
+  if (error) {
+    return <div style={{ padding: "20px", backgroundImage: "url('/assets/error.jpg')", minHeight: "100vh" }} className={styles.error}>Error: {error.message}</div>;
+  }
+
+  return (
+    <>
+      <title>StarWars</title>
+
+      <div style={{ padding: "20px", backgroundImage: "url('/assets/background.jpg')", minHeight: "100vh" }}>
+        <h1 className={styles.mainHeader}>Star Wars React Next.js AJAX Client</h1>
+
+        {loading ? (
+          <div className={styles.spinner_loading}>
+            <div className={styles.spinner}></div> {/* Спиннер */}
+          </div>
+        ) : (
+          <div className={styles.episodes}>
+            <title>StarWars</title>
+            {episodes.map((episode) => (
+              <div key={episode.episode_id} className={styles.episode}>
+                <div
+                  className={styles.episodeHeader}
+                  onClick={() => episodeClick(episode.episode_id)}
+                >
+                  <h2 className={styles.episodeTitle}>{episode.title}</h2>
+                  <h3 className={styles.episodeInfo}>
+                    Episode {episode.episode_id}, {new Date(episode.release_date).getFullYear()}
+                  </h3>
+                </div>
+                {episode.showAllInfo && (
+                  <div className={styles.showAllInfo}>
+                    {episode.opening_crawl.replaceAll("\r\n", " ")}
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
